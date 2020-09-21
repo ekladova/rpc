d49f31f354a4b5cd2216f62598f5d0958e8da6b0
Kate Kladova <ek.kladova@gmail.com>
2020-09-21 13:42:50 +0100

    Updates

---
+++ precache-manifest.f043c0601369510453a89cd0a2873adc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd4f3445fc0cf0640aea66b1b073bb3",
+    "url": "/rpc/index.html"
+  },
+  {
+    "revision": "966840efc36f0ebff565",
+    "url": "/rpc/static/css/main.3c71b10e.chunk.css"
+  },
+  {
+    "revision": "7e5bb21348a07717830b",
+    "url": "/rpc/static/js/2.9cf954f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rpc/static/js/2.9cf954f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "966840efc36f0ebff565",
+    "url": "/rpc/static/js/main.72932f5d.chunk.js"
+  },
+  {
+    "revision": "b7053fb0efb29e898fcc",
+    "url": "/rpc/static/js/runtime-main.a9ee279f.js"
+  },
+  {
+    "revision": "4e9a104260d8e81ce1415f82cee79ee9",
+    "url": "/rpc/static/media/paper.4e9a1042.png"
+  },
+  {
+    "revision": "e2cde82141a3bf580a6ca15d278b3ea5",
+    "url": "/rpc/static/media/rock.e2cde821.png"
+  },
+  {
+    "revision": "74c7a12fb4dc519ec5b27064e8198bf8",
+    "url": "/rpc/static/media/scissors.74c7a12f.png"
+  }
+]);
